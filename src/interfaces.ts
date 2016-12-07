5b07cb3aed1c382e0d24bb5013aa3e484e074f17
EvolvedCode <nickstathas@outlook.com>
2016-12-07 19:37:42 +0200

    Beautify

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,21 +1,22 @@
 export interface config {
-    backup: number,
-    evaluatorPath: string,
-    cExcess: number, //The compatibility constant for excess genes.
-    cDisjoint: number, //The compatibility constant for disjoint genes.
-    cMatching: number, //The compatibility constant for matching genes.
-    deltaThreshold: number, //The compatibility threshold.
-    cSmallGenome: number, //The maximum number of genes a genome can have to be considered small.
-    cSmallSpecies: number, //The number of members a species has in order to be considered small.
-    cStagnantSpecies: number, //The number of generations a species must remain static, in order to become stagnant.
-    cCull: number, //The fraction of its population a species will be culled to.
-    pCrossover: number, //The probability that two parents will crossover before mutating.
-    pDisable: number, //The probability a gene will be disabled if it's disabled in either parent.
-    pPerturb: number, //The probability a genome will have its connection weights perturbed.
-    pPerturbUniform: number, //If a weight is to be perturbed, it will either be uniformly perturbed or set to a new value.
-    pLink: number, //The probability of adding a new link during mutation.
-    pNeuron: number, //The probability of adding a new neuron during mutation.
-    pKeepNotFit: number, //Keep the matching Gene from the least fit Organism during crossover.
-    newWeight: () => number, //The weight a new gene will spawn with.
-    neuronActivation: (number) => number //The default activation function for all neurons.
+	backup: number,
+	evaluatorPath: string,
+	cExcess: number, //The compatibility constant for excess genes.
+	cDisjoint: number, //The compatibility constant for disjoint genes.
+	cMatching: number, //The compatibility constant for matching genes.
+	deltaThreshold: number, //The compatibility threshold.
+	cSmallGenome: number, //The maximum number of genes a genome can have to be considered small.
+	cSmallSpecies: number, //The number of members a species has in order to be considered small.
+	cStagnantSpecies: number, //The number of generations a species must remain static, in order to become stagnant.
+	cTopSpecies: number, //The minimum number of species (cannot be killed by being stagnant).
+	cCull: number, //The fraction of its population a species will be culled to.
+	pCrossover: number, //The probability that two parents will crossover before mutating.
+	pDisable: number, //The probability a gene will be disabled if it's disabled in either parent.
+	pPerturb: number, //The probability a genome will have its connection weights perturbed.
+	pPerturbUniform: number, //If a weight is to be perturbed, it will either be uniformly perturbed or set to a new value.
+	pLink: number, //The probability of adding a new link during mutation.
+	pNeuron: number, //The probability of adding a new neuron during mutation.
+	pKeepNotFit: number, //Keep the matching Gene from the least fit Organism during crossover.
+	newWeight: () => number, //The weight a new gene will spawn with.
+	neuronActivation: (number) => number //The default activation function for all neurons.
 }
--- src/modules/Helper.ts
+++ src/modules/Helper.ts
@@ -1,21 +1,21 @@
 export function randInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export function randEntry(arr: Array<any>) {
-    return arr[randInt(0, arr.length - 1)];
+	return arr[randInt(0, arr.length - 1)];
 }
 
 export function isundef(val: any): boolean {
-    return val === undefined || val === null;
+	return val === undefined || val === null;
 }
 
 export function shufflearr(a: Array<any>) {
-    let j, x, i;
-    for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
+	let j, x, i;
+	for (i = a.length; i; i--) {
+		j = Math.floor(Math.random() * i);
+		x = a[i - 1];
+		a[i - 1] = a[j];
+		a[j] = x;
+	}
 }
